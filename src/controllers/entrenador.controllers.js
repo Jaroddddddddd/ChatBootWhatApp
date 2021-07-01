a60177b02a4038a1a94e7f2dcd677e082c6e11ac
Natalia Macias Mera <56410819+Rapthalia31-01@users.noreply.github.com>
2021-07-01 14:05:47 -0500

    Update entrenador.controllers.js

--- src/controllers/entrenador.controllers.js
+++ src/controllers/entrenador.controllers.js
@@ -1,7 +1,7 @@
 const { Entrenador } = require('../model')
 
   async function mostrarEntrenador(req, res){
-    const entre = await Entrenador.find({})
+    const entre = await Entrenador.find({});
     res.json(entre)
 
 }
